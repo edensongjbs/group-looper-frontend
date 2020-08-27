c926644b804e527d994ee716085b9585f5131d48
edensongjbs <edensongjbs@gmail.com>
2020-08-27 19:15:08 -0400

    basic layout complete

--- src/containers/TransportBar.js
+++ src/containers/TransportBar.js
@@ -4,7 +4,21 @@ export default class TransportBar extends React.Component {
     render() {
         return(
             <div className="transport">
-                This is the Transport
+                <div className="song-title"><h3>Song Title</h3></div>
+                <div className="layer-name"><h4>Layer Name</h4></div>
+                <div className="playback-controls">
+                    <button>Play</button>
+                    <button>Commit</button>
+                    <button>Clear</button>
+                </div>
+                <div className="tempo-meter">Tempo and Meter</div>
+                <div className="sound-select"><p>Sound Select</p>
+                    <form>
+                        <select type="select">
+                            <option>Default Option</option>
+                        </select>
+                    </form>
+                </div>
             </div>
         )
     }
